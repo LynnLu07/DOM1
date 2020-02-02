5f7d3a9eb82bfb9c738b23ef1eb2d9553073448f
LynnLu07 <55317198+LynnLu07@users.noreply.github.com>
2020-02-02 13:34:20 +0800

    Update dom.js

--- src/dom.js
+++ src/dom.js
@@ -1,14 +1,19 @@
 window.dom = {
-    find(selector, scope){
+    find(selector,scope){
         return (scope || document).querySelectorAll(selector)
     },
-    style(node,name,value){
-        node.style[name]= value
-    },
-    each(nodeList, fn){
-        for(let i=0;i<nodeList.length;i++){
-            fn.call(null, nodeList[i])
-        }
+    style(div,object){
+    	for(let key in object){
+		div.style[key] = object[key]
+   	 }
+	return key
+   },
+    each(divList,n){
+       	for(let i =0;i<divList.length;i++){
+		n.call(null,divList[i])
+	}
+	return i
     },
 }
+
 
